60ac1f32f8b09276239a3a3b00eddf3c675999bd
Nick Fialkowski <nickolai.fialkowski@gmail.com>
2019-03-04 23:19:18 -0500

    initial commit - blank project template

---
+++ AmusementParkPassGenerator/AmusementParkPassGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmusementParkPassGenerator
+//
+//  Created by nikko444 on 2019-03-04.
+//  Copyright © 2019 nikko444. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
