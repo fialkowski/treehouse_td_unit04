7d58e4752bfd3db5289e1f8a6f9bad34d923e072
Nick Fialkowski <nikolai.fialkowski@gmail.com>
2019-07-28 21:42:56 -0400

    Added an extension that sets a background colour for UIStackView class Added custom colours UIColor extension

--- AmusementParkPassGenerator/AmusementParkPassGenerator/ViewController.swift
+++ AmusementParkPassGenerator/AmusementParkPassGenerator/ViewController.swift
@@ -8,10 +8,39 @@
 
 import UIKit
 
+extension UIStackView {
+    func setBackground(to color: UIColor) {
+        let backgroundView: UIView = {
+            let view = UIView()
+            view.backgroundColor = color
+            return view
+        }()
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        self.insertSubview(backgroundView, at: 0)
+        NSLayoutConstraint.activate([
+            backgroundView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            backgroundView.topAnchor.constraint(equalTo: self.topAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+            ])
+    }
+}
+
+extension UIColor {
+        static var topMenuBarColor: UIColor  { return UIColor(red: 138/255.0, green: 109/255.0, blue: 170/255.0, alpha: 1.0) }
+        static var subMenuBarColor: UIColor { return UIColor(red: 61/255.0, green: 55/255.0, blue: 71/255.0, alpha: 1.0) }
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topMenuBarStackView: UIStackView!
+    @IBOutlet weak var subMenuBarStackView: UIStackView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        topMenuBarStackView.setBackground(to: .topMenuBarColor)
+        subMenuBarStackView.setBackground(to: .subMenuBarColor)
         var entrants = [ParkAdmissable]()
         var passReaders = [PassReader]()
         do {
@@ -94,3 +123,31 @@ class ViewController: UIViewController {
     }
 }
 
+/* AN EXAMPLE OF AN ERROR POP-UPs
+ 
+ @IBAction func purchase() {
+ if let currentSelection = currentSelection {
+ do {
+ try vendingMachine.vend(selection: currentSelection, quantity: Int(quantityStepper.value))
+ updateDisplayWith(balance: vendingMachine.amountDeposited, totalPrice: 0.0, itemPrice: 0.0, itemQuantity: 1)
+ } catch VendingMachineError.outOfStock {
+ showAlertWith(title: "Out of Stock", message: "This item is unavailable. Please make another selection")
+ } catch VendingMachineError.insufficientFunds(let required){
+ let message = "You need $\(required) to complete the purchase"
+ showAlertWith(title: "Insufficient Funds", message: message)
+ } catch VendingMachineError.invalidSelection {
+ showAlertWith(title: "Invalid Selection", message: "Please make another selection")
+ } catch let error {
+ fatalError("\(error)")
+ }
+ 
+ if let indexPath = collectionView.indexPathsForSelectedItems?.first {
+ collectionView.deselectItem(at: indexPath, animated: true)
+ updateCell(having: indexPath, selected: false)
+ }
+ } else {
+ //FIXME: Alert user to no selection
+ }
+ }
+ 
+ */
